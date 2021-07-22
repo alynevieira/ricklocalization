7947b28bc1f21923e8eda0aecf3bd185b082f96e
alynevieira <alynealicevieira1@gmail.com>
2021-07-22 14:31:29 -0300

    Inicializando projeto e criando estrutura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DimensionsComponent } from './pages/dimensions/dimensions.component';
+import { HomeComponent } from './pages/home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'dimensions', component: DimensionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/pages/dimensions/dimensions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-dimensions',
+  templateUrl: './dimensions.component.html',
+  styleUrls: ['./dimensions.component.sass']
+})
+
+export class DimensionsComponent { }
